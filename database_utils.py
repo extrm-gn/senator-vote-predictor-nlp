9d477dd5e9611945dd2e6ba88915f7e05e034435
Marc Rosales <samtam22k@gmail.com>
2025-01-18 14:05:11 +0800

    done creating the logic of creating date data and turned it into a pandas dataframe

--- database_utils.py
+++ database_utils.py
@@ -1,19 +1,21 @@
 import psycopg2
+from datetime import date, timedelta, datetime
+import pandas as pd
 
 conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
                         password="1234", port=5432)
 
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS person (
-                id INT PRIMARY KEY,
-                name VARCHAR(255),
-                age INT,
-                gender CHAR);""")
-
-cur.execute("""INSERT INTO person (id,name,age,gender) VALUES
-                (2, 'Mike', 30, 'm')""")
-conn.commit()
+# cur.execute("""CREATE TABLE IF NOT EXISTS person (
+#                 id INT PRIMARY KEY,
+#                 name VARCHAR(255),
+#                 age INT,
+#                 gender CHAR);""")
+#
+# cur.execute("""INSERT INTO person (id,name,age,gender) VALUES
+#                 (2, 'Mike', 30, 'm')""")
+# conn.commit()
 
 cur.close()
 conn.close()
@@ -22,8 +24,38 @@ conn.close()
 def init_db():
     #TODO: create necessary tables such as the date, video, author, and comment table
 
+    start_date = datetime(2024, 10, 1)
+    end_date = datetime(2025, 5, 12)
+
+    date_list = []
+
+    while start_date < end_date:
+        date_list.append(start_date)
+        start_date += timedelta(days=1)
+
+    date_data = []
+
+    for date in date_list:
+
+        date_dict = {
+            'id': f"{date.month}{date.day}{date.year}",
+            'month': date.month,
+            'day': date.day,
+           'year': date.year
+        }
+        date_data.append(date_dict)
+
+    date_df = pd.DataFrame(date_data).set_index('id')
+    print(date_df)
+
+    create_date = """CREATE TABLE IF NOT EXISTS date (
+                     date_id PRIMARY KEY,
+                     month INT,
+                     day INT);"""
+
     return 0
 
+init_db()
 
 def insert_comments(comments):
     #TODO: Create placeholder code for the insertion of comments that would come from calling a function from another file
